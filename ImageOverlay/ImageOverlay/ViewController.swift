68d48acc6738ad7132bc5cce81d61b978bc57559
Manny Cespedes <mgcesp@gmail.com>
2015-10-07 23:37:28 -0400

    updated ImageOverlay

--- ImageOverlay/ImageOverlay/ViewController.swift
+++ ImageOverlay/ImageOverlay/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        image.image = textToImage("These are my raybans!", inImage: UIImage(named: "raybans")!, atPoint: CGPoint(x: 100,y: 100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textToImage(drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> UIImage {
+        
+        let textColor: UIColor = UIColor.whiteColor()
+        let textFont: UIFont = UIFont(name: "Helvetica Bold", size: 50)!
+        
+        UIGraphicsBeginImageContext(inImage.size)
+        
+        let textFontAttributes = [
+            NSFontAttributeName: textFont,
+            NSForegroundColorAttributeName: textColor
+        ]
+        
+        inImage.drawInRect(CGRectMake(0, 0, inImage.size.width, inImage.size.height))
+        
+        let rect: CGRect = CGRectMake(atPoint.x, atPoint.y, inImage.size.width, inImage.size.height)
+        
+        drawText.drawInRect(rect, withAttributes: textFontAttributes)
+        
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    
 }
 
